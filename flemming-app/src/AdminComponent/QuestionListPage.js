2e486e6efef74c6838b0deaeac497c35857e78d7
ARGI GOPI <rgthecodeveloper@gmail.com>
2024-06-18 09:03:47 +0530

    latest changes question list

--- flemming-app/src/AdminComponent/QuestionListPage.js
+++ flemming-app/src/AdminComponent/QuestionListPage.js
@@ -1,161 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function QuestionListPage() {
-//     const [gameMode, setGameMode] = useState('');
-//     const [college, setCollege] = useState('');
-//     const [data, setData] = useState([]);
-//     const [editIndex, setEditIndex] = useState(-1);
-//     const [editQuestion, setEditQuestion] = useState('');
-//     const [editAnswer, setEditAnswer] = useState('');
-
-//     useEffect(() => {
-//         if (gameMode && college) {
-//             const token = sessionStorage.getItem('accessToken');
-//             const backendValue = getBackendValue(gameMode);
-//             const apiUrl = `https://api-flrming.dhoomaworksbench.site/api/game/?campus_name=${college}&game_mode=${backendValue}`
-//             // const apiUrl = `https://api-flrming.dhoomaworksbench.site/api/game/?game_mode=${backendValue}&campus_name=${college}`;
-//             axios.get(apiUrl, {
-//                 headers: {
-//                     'Authorization': `Bearer ${token}`
-//                 }
-//             })
-//                 .then(response => {
-//                     setData(response.data);
-//                     console.log(response.data,'rs')
-//                 })
-//                 .catch(error => {
-//                     console.error('There was an error fetching the data!', error);
-//                 });
-//         }
-//     }, [gameMode, college]);
-
-//     const getBackendValue = (mode) => {
-//         switch (mode) {
-//             case 'Mcq Question':
-//                 return 'option';
-//             case 'File upload':
-//                 return 'image';
-//             case 'QR scanner':
-//                 return 'qr';
-//             default:
-//                 return '';
-//         }
-//     };
-
-//     const handleEdit = (index) => {
-//         setEditIndex(index);
-//         setEditQuestion(data[index].question);
-//         setEditAnswer(data[index].answer);
-//     };
-
-//     const handleSave = () => {
-//         const updatedData = [...data];
-//         updatedData[editIndex] = { ...updatedData[editIndex], question: editQuestion, answer: editAnswer };
-//         setData(updatedData);
-//         setEditIndex(-1);
-//         setEditQuestion('');
-//         setEditAnswer('');
-//     };
-
-//     const handleDelete = (index) => {
-//         const updatedData = data.filter((_, i) => i !== index);
-//         setData(updatedData);
-//     };
-
-//     return (
-//         <>
-//             <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
-//                 <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
-//                     <div className="col-md-8 d-flex justify-content-center">
-//                         <div className="card text-white p-4 rounded shadow-lg" style={{ height: '60vh', width: '100%', overflowY: 'auto', maxWidth: '600px' }}>
-//                             <div className="form-group">
-//                                 <label htmlFor="gameModeSelect">Select Game Mode</label>
-//                                 <select
-//                                     id="gameModeSelect"
-//                                     className="form-control"
-//                                     value={gameMode}
-//                                     onChange={(e) => setGameMode(e.target.value)}
-//                                 >
-//                                     <option value="">Select Game Mode</option>
-//                                     <option value="Mcq Question">Mcq Question</option>
-//                                     <option value="File upload">File upload</option>
-//                                     <option value="QR scanner">QR scanner</option>
-//                                 </select>
-//                             </div>
-//                             <div className="form-group mt-3">
-//                                 <label htmlFor="collegeSelect">Select College</label>
-//                                 <select
-//                                     id="collegeSelect"
-//                                     className="form-control"
-//                                     value={college}
-//                                     onChange={(e) => setCollege(e.target.value)}
-//                                 >
-//                                     <option value="">Select College</option>
-//                                     <option value="fleming">Sutherland</option>
-//                                     <option value="Lindsay">Lindsay</option>
-//                                     <option value="Haliburton">Haliburton</option>
-//                                 </select>
-//                             </div>
-//                             <table className="table table-striped table-light mt-4">
-//                                 <thead>
-//                                     <tr>
-//                                         <th>Question</th>
-//                                         <th>Answer</th>
-//                                         <th>Actions</th>
-//                                     </tr>
-//                                 </thead>
-//                                 {/* <tbody>
-//                                     {data.map((item, index) => (
-//                                         <tr key={index}>
-//                                             <td>
-//                                                 {editIndex === index ? (
-//                                                     <input
-//                                                         type="text"
-//                                                         value={editQuestion}
-//                                                         onChange={(e) => setEditQuestion(e.target.value)}
-//                                                     />
-//                                                 ) : (
-//                                                     item.question
-//                                                 )}
-//                                             </td>
-//                                             <td>
-//                                                 {editIndex === index ? (
-//                                                     <input
-//                                                         type="text"
-//                                                         value={editAnswer}
-//                                                         onChange={(e) => setEditAnswer(e.target.value)}
-//                                                     />
-//                                                 ) : (
-//                                                     item.answer
-//                                                 )}
-//                                             </td>
-//                                             <td>
-//                                                 {editIndex === index ? (
-//                                                     <button className="btn btn-success" onClick={handleSave}>Save</button>
-//                                                 ) : (
-//                                                     <button className="btn btn-primary" onClick={() => handleEdit(index)}>Edit</button>
-//                                                 )}
-//                                                 <button className="btn btn-danger ml-2" onClick={() => handleDelete(index)}>Delete</button>
-//                                             </td>
-//                                         </tr>
-//                                     ))}
-//                                 </tbody> */}
-//                             </table>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default QuestionListPage;
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -252,12 +94,12 @@ function QuestionListPage() {
     };
 
     return (
-        <>
-            <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
-                <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
-                    <div className="col-md-8 d-flex justify-content-center">
-                        <div className="card text-white p-4 rounded shadow-lg" style={{ height: '60vh', width: '100%', overflowY: 'auto', maxWidth: '600px' }}>
-                            <div className="form-group mb-5">
+        <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
+            <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
+                <div className="col-md-8">
+                    <div className="card text-white p-4 rounded shadow-lg" style={{ overflowY: 'auto', maxHeight: '80vh' }}>
+                        <div className="form-row mb-4">
+                            <div className="form-group col-md-6">
                                 <label htmlFor="gameModeSelect">Select Game Mode</label>
                                 <select
                                     id="gameModeSelect"
@@ -271,7 +113,7 @@ function QuestionListPage() {
                                     <option value="QR scanner">QR scanner</option>
                                 </select>
                             </div>
-                            <div className="form-group mt-3 mb-5">
+                            <div className="form-group col-md-6">
                                 <label htmlFor="collegeSelect">Select College</label>
                                 <select
                                     id="collegeSelect"
@@ -285,7 +127,8 @@ function QuestionListPage() {
                                     <option value="Haliburton">Haliburton</option>
                                 </select>
                             </div>
-                            <div className="mt-5">
+                        </div>
+                        {data.length > 0 && (
                             <table className="table table-striped table-light mt-4">
                                 <thead>
                                     <tr>
@@ -301,6 +144,7 @@ function QuestionListPage() {
                                                 {editIndex === index ? (
                                                     <input
                                                         type="text"
+                                                        className="form-control"
                                                         value={editQuestion}
                                                         onChange={(e) => setEditQuestion(e.target.value)}
                                                     />
@@ -312,41 +156,36 @@ function QuestionListPage() {
                                                 {editIndex === index ? (
                                                     <input
                                                         type="text"
+                                                        className="form-control"
                                                         value={editAnswer}
                                                         onChange={(e) => setEditAnswer(e.target.value)}
                                                     />
                                                 ) : (
-                                                    <ul>
+                                                    <ul className="pl-3">
                                                         {item.description.split('\n').map((line, i) => (
                                                             <li key={i}>{line}</li>
                                                         ))}
                                                     </ul>
                                                 )}
                                             </td>
-                                            <td>
+                                            <td className="d-flex">
                                                 {editIndex === index ? (
-                                                    <button className="btn btn-success" onClick={handleSave}>Save</button>
+                                                    <button className="btn btn-success btn-sm mr-2" onClick={handleSave}>Save</button>
                                                 ) : (
-                                                    <button className="btn btn-primary" onClick={() => handleEdit(index)}>Edit</button>
+                                                    <button className="btn btn-primary btn-sm mr-2" onClick={() => handleEdit(index)}>Edit</button>
                                                 )}
-                                                <button className="btn btn-danger ml-2" onClick={() => handleDelete(index)}>Delete</button>
+                                                <button className="btn btn-danger btn-sm" onClick={() => handleDelete(index)}>Delete</button>
                                             </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
 export default QuestionListPage;
-
-
-
-
-
