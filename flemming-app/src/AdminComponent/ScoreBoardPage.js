458c05f7c79694cf3606d50a5507e6bf6fbccbd0
ARGI GOPI <rgthecodeveloper@gmail.com>
2024-07-04 10:04:14 +0530

    delete button implemented

--- flemming-app/src/AdminComponent/ScoreBoardPage.js
+++ flemming-app/src/AdminComponent/ScoreBoardPage.js
@@ -1,3 +1,74 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Header from '../CommonComponent/Header';
+// import Footer from '../CommonComponent/Footer';
+
+// function ScoreBoardPage() {
+//     const [scores, setScores] = useState([]);
+//     const token = sessionStorage.getItem('accessToken');
+
+//     useEffect(() => {
+//         const fetchScores = async () => {
+//             try {
+//                 const response = await axios.get('https://api-flrming.dhoomaworksbench.site/user-score', {
+//                     headers: {
+//                         Authorization: `Bearer ${token}`,
+//                     },
+//                 });
+//                 setScores(response.data);
+//                 console.log(response.data, 'ppp')
+//             } catch (error) {
+//                 console.error('Error fetching scores:', error);
+//             }
+//         };
+
+//         fetchScores();
+//     }, [token]);
+
+//     const handleResetGame = async () => {}
+//     return (
+//         <>
+//             <Header />
+//             <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
+//                 <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
+//                     <div className="col-md-10 d-flex flex-column align-items-center" style={{ marginBottom: '100px' }}>
+                      
+//                         <div className='row'>
+//                             <div className='col-6'>
+//                             <h3 className="text-black"  style={{ marginLeft: '-31px' }}>Score Board</h3>
+//                             </div>
+//                             <div className='col-6'  style={{ marginLeft: '0px', marginTop: '4px' }}>
+//                             <button style={{ height: '53px' }} className="btn btn-danger w-100" onClick={handleResetGame}>ResetGame</button>
+//                             </div>
+//                         </div>
+//                         <table className="table table-striped table-light mt-4">
+//                             <thead>
+//                                 <tr>
+//                                     <th>Name</th>
+//                                     <th>Status</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 {/* {scores.map((score, index) => (
+//                                     <tr key={index}>
+//                                         <td>{score.name}</td>
+//                                         <td>{score.status}</td>
+//                                     </tr>
+//                                 ))} */}
+//                             </tbody>
+//                         </table>
+//                     </div>
+//                 </div>
+//             </div>
+//             <Footer />
+//         </>
+//     );
+// }
+
+// export default ScoreBoardPage;
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../CommonComponent/Header';
@@ -25,13 +96,33 @@ function ScoreBoardPage() {
         fetchScores();
     }, [token]);
 
+    const handleResetGame = async () => {
+        try {
+            await axios.get('https://api-flrming.dhoomaworksbench.site/game-reset', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log('Game reset successfully');
+            // Optionally, fetch updated scores or update local state after reset
+        } catch (error) {
+            console.error('Error resetting game:', error);
+        }
+    };
     return (
         <>
             <Header />
             <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
-                <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
-                    <div className="col-md-10 d-flex flex-column align-items-center" style={{ marginBottom: '100px' }}>
-                        <h3 className="text-black">Score Board</h3>
+                <div className="row justify-content-center mt-4" style={{ minHeight: 'calc(100vh - 181px)' }}>
+                    <div className="col-md-10 d-flex flex-column ">
+                        <div className='row'>
+                            <div className='col-md-6'>
+                                <h3 className="text-black">Score Board</h3>
+                            </div>
+                            <div className='col-md-6 d-flex justify-content-end'>
+                                <button className="btn btn-danger" onClick={handleResetGame}>Reset Game</button>
+                            </div>
+                        </div>
                         <table className="table table-striped table-light mt-4">
                             <thead>
                                 <tr>
--- flemming-app/src/AdminComponent/StudentListPage.js
+++ flemming-app/src/AdminComponent/StudentListPage.js
@@ -1,11 +1,101 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import {useNavigate } from 'react-router-dom'; 
+// import Header from '../CommonComponent/Header';
+// import Footer from '../CommonComponent/Footer';
+
+// function StudentListPage() {
+//     const [selectedCampus, setSelectedCampus] =useState(['Sutherland']) // Set default campus to Sutherland
+//     const [students, setStudents] = useState([]);
+//     const campuses = ['Sutherland', 'Lindsay', 'Haliburton', 'fleming'];
+//     const token = sessionStorage.getItem('accessToken');
+//     const navigate = useNavigate();
+
+//     const handleCampusChange = (event) => {
+//         setSelectedCampus(event.target.value);
+//     };
+
+//     useEffect(() => {
+//         const fetchStudents = async () => {
+//             if (selectedCampus) {
+//                 try {
+//                     const response = await axios.get(`https://api-flrming.dhoomaworksbench.site/api/student/?campus_name=${selectedCampus}`, {
+//                         headers: {
+//                             'Authorization': `Bearer ${token}`
+//                         }
+//                     });
+//                     setStudents(response.data.results); // Use response.data.results to get the students array
+//                 } catch (error) {
+//                     console.error('Error fetching students:', error);
+//                 }
+//             }
+//         };
+
+//         fetchStudents();
+//     }, [selectedCampus, token]); // Call the API on initial render and when selectedCampus changes
+
+//     const handleViewClick = (id) => {
+//         navigate(`/student-detail/${id}`); // Navigate to the student detail page
+//     };
+
+//     return (
+//         <>
+//             <Header />
+//             <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
+//                 <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
+//                     <div className="col-md-10 d-flex flex-column align-items-center" style={{marginBottom:'100px'}}>
+//                         <h3 className="text-black">Student List</h3>
+//                         <div className="form-group mt-4 custom-select">
+//                             <label htmlFor="campusSelect" className="text-black">Select Campus:</label>
+//                             <select id="campusSelect" className="form-control" value={selectedCampus} onChange={handleCampusChange}>
+//                                 <option value="" disabled>Select a campus</option>
+//                                 {campuses.map((campus, index) => (
+//                                     <option key={index} value={campus}>{campus}</option>
+//                                 ))}
+//                             </select>
+//                         </div>
+//                         <table className="table table-striped table-light mt-4">
+//                             <thead>
+//                                 <tr>
+//                                     <th>Username</th>
+//                                     <th>Email</th>
+//                                     <th>Contact Number</th>
+//                                     <th>Date Joined</th>
+//                                     <th>Action</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 {students.map((student, index) => (
+//                                     <tr key={index}>
+//                                         <td>{student.username}</td>
+//                                         <td>{student.email}</td>
+//                                         <td>{student.contact_number}</td>
+//                                         <td>{new Date(student.date_joined).toLocaleDateString()}</td>
+//                                         <td>
+//                                             <button className="btn btn-primary" onClick={() => handleViewClick(student.id)}>View</button>
+//                                         </td>
+//                                     </tr>
+//                                 ))}
+//                             </tbody>
+//                         </table>
+//                     </div>
+//                 </div>
+//             </div>
+//             <Footer />
+//         </>
+//     );
+// }
+
+// export default StudentListPage;
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import Header from '../CommonComponent/Header';
 import Footer from '../CommonComponent/Footer';
 
 function StudentListPage() {
-    const [selectedCampus, setSelectedCampus] =useState(['Sutherland']) // Set default campus to Sutherland
+    const [selectedCampus, setSelectedCampus] = useState('Sutherland'); // Set default campus to Sutherland
     const [students, setStudents] = useState([]);
     const campuses = ['Sutherland', 'Lindsay', 'Haliburton', 'fleming'];
     const token = sessionStorage.getItem('accessToken');
@@ -15,22 +105,22 @@ function StudentListPage() {
         setSelectedCampus(event.target.value);
     };
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            if (selectedCampus) {
-                try {
-                    const response = await axios.get(`https://api-flrming.dhoomaworksbench.site/api/student/?campus_name=${selectedCampus}`, {
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    });
-                    setStudents(response.data.results); // Use response.data.results to get the students array
-                } catch (error) {
-                    console.error('Error fetching students:', error);
-                }
+    const fetchStudents = async () => {
+        if (selectedCampus) {
+            try {
+                const response = await axios.get(`https://api-flrming.dhoomaworksbench.site/api/student/?campus_name=${selectedCampus}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                setStudents(response.data.results); // Use response.data.results to get the students array
+            } catch (error) {
+                console.error('Error fetching students:', error);
             }
-        };
+        }
+    };
 
+    useEffect(() => {
         fetchStudents();
     }, [selectedCampus, token]); // Call the API on initial render and when selectedCampus changes
 
@@ -38,21 +128,60 @@ function StudentListPage() {
         navigate(`/student-detail/${id}`); // Navigate to the student detail page
     };
 
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`https://api-flrming.dhoomaworksbench.site/api/game/${id}/`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            fetchStudents(); // Refresh the student list after deletion
+        } catch (error) {
+            console.error('Error deleting student:', error);
+        }
+    };
+
+    const handleDeleteCampus = async () => {
+        try {
+            await axios.delete(`https://api-flrming.dhoomaworksbench.site/api/student/delete-all/`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            setSelectedCampus(''); // Reset campus selection after deletion
+            setStudents([]); // Clear the student list
+        } catch (error) {
+            console.error('Error deleting campus:', error);
+        }
+    };
+
     return (
         <>
             <Header />
             <div className="container-fluid bg-gradient" style={{ overflow: 'hidden' }}>
                 <div className="row justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 181px)' }}>
-                    <div className="col-md-10 d-flex flex-column align-items-center" style={{marginBottom:'100px'}}>
-                        <h3 className="text-black">Student List</h3>
-                        <div className="form-group mt-4 custom-select">
-                            <label htmlFor="campusSelect" className="text-black">Select Campus:</label>
-                            <select id="campusSelect" className="form-control" value={selectedCampus} onChange={handleCampusChange}>
-                                <option value="" disabled>Select a campus</option>
-                                {campuses.map((campus, index) => (
-                                    <option key={index} value={campus}>{campus}</option>
-                                ))}
-                            </select>
+                    <div className="col-12 col-md-10 d-flex flex-column align-items-center" style={{ marginBottom: '100px' }}>
+                        <h3 className="text-black mt-3">Student List</h3>
+                        <div className="row w-100 mt-4">
+                            <div className="col-12 col-md-8 mb-2 mb-md-0">
+                                <div className="form-group custom-select" style={{ marginLeft: '-31px' }}>
+                                    <label htmlFor="collegeSelect" className="text-black">Select Campus:</label>
+                                    <select
+                                        id="collegeSelect"
+                                        className="form-control"
+                                        value={selectedCampus}
+                                        onChange={handleCampusChange}
+                                    >
+                                        <option value="" disabled>Select a campus</option>
+                                        <option value="fleming">Sutherland</option>
+                                        <option value="Lindsay">Lindsay</option>
+                                        <option value="Haliburton">Haliburton</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-4 text-md-right" style={{ marginLeft: '0px', marginTop: '4px' }}>
+                                <button style={{ height: '53px' }} className="btn btn-danger w-100" onClick={handleDeleteCampus}>Delete Student List</button>
+                            </div>
                         </div>
                         <table className="table table-striped table-light mt-4">
                             <thead>
@@ -61,7 +190,7 @@ function StudentListPage() {
                                     <th>Email</th>
                                     <th>Contact Number</th>
                                     <th>Date Joined</th>
-                                    <th>Action</th>
+                                    <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -72,7 +201,8 @@ function StudentListPage() {
                                         <td>{student.contact_number}</td>
                                         <td>{new Date(student.date_joined).toLocaleDateString()}</td>
                                         <td>
-                                            <button className="btn btn-primary" onClick={() => handleViewClick(student.id)}>View</button>
+                                            <button className="btn btn-primary ms-2" onClick={() => handleViewClick(student.id)}>View</button>
+                                            <button className="btn btn-danger" onClick={() => handleDelete(student.id)}>Delete</button>
                                         </td>
                                     </tr>
                                 ))}
